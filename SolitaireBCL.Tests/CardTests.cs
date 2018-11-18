5158e7c28b15f9fcc3c7c6efc1b22aadd115ddaf
Pavel <chpv24fev@gmail.com>
2018-11-18 13:41:18 +0300

    Card class was added.
    Information about cards and tests for cards was added.

---
+++ SolitaireBCL.Tests/CardTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace SolitaireBCL.Tests
+{
+    [TestFixture]
+    public class CardTests
+    {
+        [TestCase(CardSuit.Clovers, CardColour.Black)]
+        [TestCase(CardSuit.Diamonds, CardColour.Red)]
+        [TestCase(CardSuit.Hearts, CardColour.Red)]
+        [TestCase(CardSuit.Pikes, CardColour.Black)]
+        public void GetCardColourTest(CardSuit cardSuit, CardColour expectedColour)
+        {
+            //Arrange
+            Card card = new Card(cardSuit, CardValue.Ace);
+
+            //Act
+            CardColour result = card.GetCardColour();
+
+            //Assert
+            Assert.AreEqual(expectedColour, result);
+        }
+    }
+}
---
+++ SolitaireBCL/Card.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolitaireBCL
+{
+
+    public enum CardColour { Red, Black }
+    public enum CardSuit { Pikes, Hearts, Clovers, Diamonds }
+    public enum CardValue { Ace = 1, Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Jack, Queen, King }
+     
+    public static class CardInfo
+    {
+        public static Dictionary<CardSuit, CardColour> Cards = new Dictionary<CardSuit, CardColour>()
+        {
+            { CardSuit.Clovers, CardColour.Black },
+            { CardSuit.Diamonds, CardColour.Red },
+            { CardSuit.Hearts, CardColour.Red },
+            { CardSuit.Pikes, CardColour.Black }
+        };
+    }
+
+    public class Card
+    {
+        public readonly CardSuit suit;
+        public readonly CardValue value;
+
+        public Card(CardSuit suit, CardValue value)
+        {
+            this.suit = suit;
+            this.value = value;
+        }
+
+        public CardColour GetCardColour()
+        {
+            return CardInfo.Cards[suit];
+        }
+    }
+}
