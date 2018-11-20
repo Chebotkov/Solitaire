f6e82bf4d09b93d6798e54441cc8e0486ae24597
Pavel <chpv24fev@gmail.com>
2018-11-20 21:09:55 +0300

    Cards was added.

---
+++ SolitaireGUI/Additional Classes/CardManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SolitaireBCL;
+
+namespace SolitaireGUI.Additional_Classes
+{
+    public static class CardManager
+    {
+        public static string pathToCards = @"Resourses\Cards";
+        public static string GetPathToCard(Card card)
+        {
+            return String.Format("{0}\\{1}{2}.png", pathToCards, card.value.ToString(), card.suit.ToString());
+        }
+    }
+}
